7763f26b6e85846347101752fbca88f5e5b1a414
sniekerz <85154061+sniekerz@users.noreply.github.com>
2022-11-09 15:39:34 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import Info from "./components/Info";
-import About from "./components/FeeCalculator";
-import Footer from "./components/Footer";
+import FeeCalculator from "./components/FeeCalculator";
 
 export default function App() {
   return (
     <div className="full-content">
       <div className="content">
-        <Info />
-        <About />
+        <FeeCalculator />
       </div>
-      <Footer />
     </div>
   );
 }
--- src/components/FeeCalculator.js
+++ src/components/FeeCalculator.js
@@ -1,19 +1,54 @@
 import React from "react";
 
-export default function About() {
+const txAmount = 1000;
+
+export default function FeeCalculator() {
   return (
     <main>
-      <h2 className="about--title">About</h2>
-      <p className="about--text">
-        I am a frontend developer with a particular interest in making things
-        simple and automating daily tasks. I try to keep up with security and
-        best practices, and am always looking for new things to learn.
-      </p>
-      <h2 className="about--title">Interests</h2>
-      <p className="about--text">
-        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
-        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
-      </p>
+      <h2 className="about--title">Average Fee Calculator</h2>
+      <div className="input--boxes">
+        <div className="input-row">
+          <input
+            type={"number"}
+            placeholder={"Enter amount"}
+            className="input--field"
+          />
+          <div>
+            <h4 className="input--title">Transactions</h4>
+          </div>
+        </div>
+        <div className="input-row">
+          <input
+            type={"number"}
+            placeholder={"Value of transactions"}
+            className="input--field"
+          />
+          <div>
+            <h4 className="input--title">Total Value</h4>
+          </div>
+        </div>
+        <div className="input-row">
+          <input
+            type={"number"}
+            placeholder={"Percentage for pool"}
+            className="input--field"
+          />
+          <div>
+            <h4 className="input--title">Pool Percentage</h4>
+          </div>
+        </div>
+      </div>
+      <div className="center-button">
+        <button id="calculate-average-fee">Calculate</button>
+      </div>
+      <h3 className="about--title">Results</h3>
+      <div className="text--box">
+        <p className="about--text">
+          You have set a total of {txAmount} transactions <br></br>
+          You have set a total value of ${txAmount} in transactions <br></br>
+          You have set a total of {txAmount} transactions <br></br>
+        </p>
+      </div>
     </main>
   );
 }
